fe5d0a960c9af858b62b84990f150776ed831ef3
Pasinee Rukpromrach <64160056@go.buu.ac.th>
2022-12-29 23:00:21 +0700

    add user data

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,4 +1,12 @@
+import type { User } from "@/type/User";
 import { defineStore } from "pinia";
 import { computed, ref } from "vue";
 
-export const useUserStore = defineStore("user", () => {});
+export const useUserStore = defineStore("user", () => {
+  const lastId = 3;
+  const user = ref<User[]>([
+    { id: 1, login: "admin", name: "Administrator", password: "Pass@1234" },
+    { id: 2, login: "user1", name: "User1", password: "Pass@1234" },
+  ]);
+  return { user };
+});
