c5d3335b1ed42be136200d2f565d5a13d869cf58
Pasinee Rukpromrach <64160056@go.buu.ac.th>
2022-12-30 23:22:53 +0700

    add card

--- src/views/user/UserCardList.vue
+++ src/views/user/UserCardList.vue
@@ -1,7 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useUserStore } from "../../stores/user";
+import { mdiDelete, mdiPencil, mdiPlus } from "@mdi/js";
+import type { User } from "@/type/User";
+const userStore = useUserStore();
+
+const deleteUser = (index: number): void => {
+  userStore.deleteUser(index);
+};
+const addNewUser = () => {
+  userStore.dialog = true;
+};
+const editUser = (user: User) => {
+  userStore.editUser(user);
+};
+</script>
 
 <template>
-  <div></div>
+  <v-container>
+    <v-row>
+      <v-col cols="12" md="4" v-for="item of userStore.users" :key="item.id">
+        <v-card>
+          <v-card-title> {{ item.id }} {{ item.login }} </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <style scoped></style>
--- src/views/user/UserView.vue
+++ src/views/user/UserView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import UserCardList from "./UserCardList.vue";
 import UserDialog from "./UserDialog.vue";
 import UserList from "./UserList.vue";
 </script>
@@ -7,6 +8,7 @@ import UserList from "./UserList.vue";
   <div class="text-h1"></div>
   <UserList></UserList>
   <UserDialog></UserDialog>
+  <UserCardList></UserCardList>
 </template>
 
 <style scoped></style>
